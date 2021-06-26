1c7915c180b0e7a106737853090d3ce1bae2e50e
Tejesh <tejesh.allampati@zemosolabs.com>
2021-06-26 16:59:40 +0530

    Added styling

--- src/components/organisms/Banner/Banner.styles.tsx
+++ src/components/organisms/Banner/Banner.styles.tsx
@@ -2,4 +2,22 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core';
 
 export const BannerStyles = makeStyles((theme) => {
+
+    return {
+        root: {
+            maxHeight: '300px',
+            maxWidth: '900px',
+            margin: '0 auto',
+            overflow: 'hidden',
+        },
+        slider: {
+            width: '100%',
+            whiteSpace: 'no-wrap',
+        },
+        image: {
+            width: '100%',
+            height: '300px',
+            display: 'inline-block',
+        }
+    }
 });
--- src/components/organisms/Banner/index.tsx
+++ src/components/organisms/Banner/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { BannerStyles } from './Banner.styles';
 
 export type BannerProps = {
     images: string[]
@@ -6,11 +9,29 @@ export type BannerProps = {
 
 export const Banner = ({images} : BannerProps) => {
 
+    const classes = BannerStyles()
+    // Slide show delay
+    const delay = 250
+
+    let [index, setIndex] = useState(0)
+
+    useEffect(() => {
+        setTimeout(
+          () =>
+            setIndex((prevIndex) =>
+              prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+            ),
+          delay
+        );
+        return () => {};
+      }, [index]);
+    
+
     return (
-        <div>
-            <div>
+        <div className={classes.root}>
+            <div className={classes.slider} style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
                 {images.map((image, id) => {
-                    <img src={image}/>
+                    return <img key={`${id}img`} src={image} className={classes.image}/>
                 })}
             </div>
         </div>
